a3ea3f525ca0fb12393d2cfbd286b61fcd813a3a
enochmakafui <enochmack04@gmail.com>
2023-10-13 01:48:13 -1200

    Complete music player

---
+++ index.js
@@ -0,0 +1,28 @@
+// $("h1").addClass("heading");
+// $("h1").removeClass("heading");
+
+
+// $("h1").text("World");
+// $("h1").css("color","red");
+
+// $("a").attr("href","https://mail.yahoo.com")
+
+// let name = "";
+
+
+// $("input").keypress(function(event){
+ 
+//     const keyPressed = event.key;
+//     name += keyPressed
+// })
+// $("button").click(function(){
+  
+//     $("h1").text(`Hello ${name}`);
+// })
+
+$("button").click(function(){
+    let song = new Audio("song.mp3");
+    song.play();
+})
+
+
